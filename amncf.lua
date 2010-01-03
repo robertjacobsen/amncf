1014cad666cbfffbe1f3eac01cf2edb909d7a40d
Robert Jacobsen <robert@robertjacobsen.net>
2010-01-03 19:18:40 +0100

    Rewrote much of the functionality to allow a more generic and useful frame. Now follows ChatFrame1. Cleaner code.

--- amncf.lua
+++ amncf.lua
@@ -20,7 +20,6 @@ end
  Function MakeFrame 
 -- The function that makes a frame with the given options taken from the arguments
 -- Parameters:
--- name - Name of the frame
 -- width - The width of the frame
 -- height - The height of the frame
 -- r - Amount of red in the colour of the frame
@@ -28,8 +27,8 @@ end
 -- b - Amount of blue in the colour of the frame
 -- a - Alpha (transparency) of the frame
 --]]
-local MakeFrame = function(name, width, height, r, g, b, a) 
-	local frame = CreateFrame("Frame", name, UIParent)
+local MakeFrame = function(r, g, b, a) 
+	local frame = CreateFrame("Frame", nil, UIParent)
 
 
 	frame:SetBackdrop({
@@ -37,8 +36,6 @@ local MakeFrame = function(name, width, height, r, g, b, a)
 		edgeFile = "Interface\\Tooltips\\UI-Tooltip-Border", edgeSize = 16,
 		insets = {left = 0, right = 0, top = 0, bottom = 0},
 	})
-	frame:SetHeight(height)
-	frame:SetWidth(width)
 	frame:SetBackdropColor(r, g, b, a)
 	frame:SetBackdropBorderColor(0, 0, 0, 0)
 	frame:SetFrameStrata"BACKGROUND"
@@ -50,64 +47,42 @@ amncf:RegisterEvent("PLAYER_LOGIN")
 amncf:SetScript("OnEvent", function(self) 
 	--[[ The left end side of the screen, chat frames etc ]]
 
-	-- ChatFrame
-	local cf = MakeFrame("CF", 550, 216, 0, 0, 0, 0.7)
-	cf:SetPoint("BOTTOMLEFT", UIParent, "BOTTOMLEFT", 19, 25)
 	FixChat() -- We don't like textures!
-
---[[	-- ChatFrameBorderLeft
-	local cfbl = MakeFrame("CFBL", 1, cf:GetHeight()+2, 0, 0, 0, 1)
-	cfbl:SetPoint("RIGHT", cf, "LEFT")
-
-	-- ChatFrameBorderRight
-	local cfbr = MakeFrame("CFBR", 1, cf:GetHeight()+2, 0, 0, 0, 1)
-	cfbr:SetPoint("LEFT", cf, "RIGHT")
-
-	-- ChatFrameBorderTop
-	local cfbt = MakeFrame("CFBT", cf:GetWidth(), 1, 0, 0, 0, 1)
-	cfbt:SetPoint("BOTTOM", cf, "TOP")
-
-	-- ChatFrameBorderBottom
-	local cfbb = MakeFrame("CFBB", cf:GetWidth(), 1, 0, 0, 0, 1)
-	cfbb:SetPoint("TOP", cf, "BOTTOM") ]] 
-	-- ChatFrameUpperLeft
-	local cful = MakeFrame("CFUL", 2, cf:GetHeight(), 0.243, 0.298, 0.345, 0.7)
-	cful:SetPoint("RIGHT", cf, "LEFT")
-
-	-- ChatFrameUpperRight
-	local cfur = MakeFrame("CFUR", 2, cf:GetHeight(), 0.243, 0.298, 0.345, 0.7)
-	cfur:SetPoint("LEFT", cf, "RIGHT")
-
-	-- ChatFrameUpperTop
-	local cfut = MakeFrame("CFUT", cf:GetWidth()+4, 2, 0.243, 0.298, 0.345, 0.7)
-	cfut:SetPoint("BOTTOM", cf, "TOP")
-
-	-- ChatFrameUpperBottom
-	local cfub = MakeFrame("CFUB", cf:GetWidth()+4, 2, 0.243, 0.298, 0.345, 0.7)
-	cfub:SetPoint("TOP", cf, "BOTTOM")
 	
---[[	-- ChatFrameUpperBorderLeft
-	local cfubl = MakeFrame("CFUBL", 1, cful:GetHeight()+6, 0, 0, 0, 1)
-	cfubl:SetPoint("RIGHT", cful, "LEFT")
+    -- ChatFrame
+	local cf = MakeFrame(0, 0, 0, 0.7)
 
-	-- ChatFrameUpperBorderRight
-	local cfubr = MakeFrame("CUFBR", 1, cfur:GetHeight()+6, 0, 0, 0, 1)
-	cfubr:SetPoint("LEFT", cfur, "RIGHT")
+	-- ChatFrameLeft
+	local cfl = MakeFrame(0.243, 0.298, 0.345, 0.7)
+    cfl:SetWidth(2)
+	cfl:SetPoint("RIGHT", cf, "LEFT")
+    cfl:SetPoint("TOP", cf)
+    cfl:SetPoint("BOTTOM", cf)
+	-- ChatFrameRight
+	local cfr = MakeFrame(0.243, 0.298, 0.345, 0.7)
+    cfr:SetWidth(2)
+	cfr:SetPoint("LEFT", cf, "RIGHT")
+    cfr:SetPoint("TOP", cf)
+    cfr:SetPoint("BOTTOM", cf)
 
-	-- ChatFrameUpperBorderTop
-	local cfubt = MakeFrame("CFUBT", cfut:GetWidth(), 1, 0, 0, 0, 1)
-	cfubt:SetPoint("BOTTOM", cfut, "TOP")
+    -- ChatFrameTop
+	local cft = MakeFrame(0.243, 0.298, 0.345, 0.7)
+    cft:SetHeight(2)
+	cft:SetPoint("BOTTOM", cf, "TOP")
+    cft:SetPoint("LEFT", cf, -2, 0)
+    cft:SetPoint("RIGHT", cf, 2, 0)
 
-	-- ChatFrameUpperBorderBottom
-	local cfubb = MakeFrame("CFUBB", cfub:GetWidth(), 1, 0, 0, 0, 1)
-	cfubb:SetPoint("TOP", cfub, "BOTTOM") ]]--
-
+	-- ChatFrameBottom
+	local cfb = MakeFrame(0.243, 0.298, 0.345, 0.7)
+    cfb:SetHeight(2)
+	cfb:SetPoint("TOP", cf, "BOTTOM")
+    cfb:SetPoint("LEFT", cf, -2, 0)
+    cfb:SetPoint("RIGHT", cf, 2, 0)
+	
 	-- Fixes and locks the chat frame into place.
-	ChatFrame1:SetWidth(cf:GetWidth()-10)
-	ChatFrame1:ClearAllPoints()
-	ChatFrame1:SetPoint("BOTTOM", cf, 0, 2)
-	ChatFrame1:SetPoint("TOP", cf, 0, 0)
-	ChatFrame1:SetPoint("LEFT", cf, 2, 0)
-	FCF_SetLocked(ChatFrame1, 1)
+	cf:SetPoint("TOP", ChatFrame1, 0, 2)
+    cf:SetPoint("BOTTOM", ChatFrame1, 0, -2)
+    cf:SetPoint("LEFT", ChatFrame1, -2, 0)
+    cf:SetPoint("RIGHT", ChatFrame1, 2, 0)
 end)
 
